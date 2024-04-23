69bdc4b3f9255a9ee0e1172eb32abe5ec9da30c2
pshistory <psh_a@naver.com>
2024-04-23 16:34:31 +0900

    join해서 쓰는 user_id 인덱스 적용

--- src/main/java/com/popcorntalk/domain/user/entity/User.java
+++ src/main/java/com/popcorntalk/domain/user/entity/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,7 +21,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "users")
+@Table(name = "users", indexes = @Index(name = "idx_user_id", columnList = "id"))
 public class User {
 
     @Id
